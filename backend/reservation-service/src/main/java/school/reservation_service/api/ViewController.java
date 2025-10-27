082f488be2d8ab4d89f4afa8660c9ac5a0130fe3
YuWonJong00 <wonbell00@gmail.com>
2025-10-27 12:53:26 +0900

    view

---
+++ backend/reservation-service/src/main/java/school/reservation_service/api/ViewController.java
@@ -0,0 +1,19 @@
+package school.reservation_service.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+    @GetMapping({"/","/login"})
+    public String login(){
+        return "forward:/login.html";
+    }
+    @GetMapping({"/facilities"})
+    public String facilities() {
+        return "forward:/facilities.html";
+    }
+
+
+
+}
