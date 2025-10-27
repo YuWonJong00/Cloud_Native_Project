5d1bcc998e2a09da666002d249927419af020e82
YuWonJong00 <wonbell00@gmail.com>
2025-10-27 11:47:06 +0900

    Exception

---
+++ backend/reservation-service/src/main/java/school/reservation_service/support/AlreadyReservedException.java
@@ -0,0 +1,13 @@
+package school.reservation_service.support;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AlreadyReservedException extends RuntimeException {
+    public AlreadyReservedException(String message) {
+        super(message);
+        log.error(message);
+
+
+    }
+}
